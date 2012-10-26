f18dc9083d284672036f214602980cabe3d33490
Mick Koch <kchmck@gmail.com>
2012-10-26 18:13:44 -0400

    Add cin input to ALU4Bit

--- ALU4Bit.v
+++ ALU4Bit.v
@@ -1,8 +1,9 @@
 `timescale 1ns / 1ps
 
-module ALU4Bit(a, b, op, result, cout, G, P, set, overflow, zero);
+module ALU4Bit(a, b, op, cin, result, cout, G, P, set, overflow, zero);
   input [3:0] a, b;
   input [2:0] op;
+  input cin;
   output [3:0] result;
   output cout;
   output G, P;
@@ -16,7 +17,7 @@ module ALU4Bit(a, b, op, result, cout, G, P, set, overflow, zero);
 
   assign zero = ~result[3] & ~result[2] & ~result[1] & ~result[0];
 
-  ALU1Bit alu1(a[0], b[0], op[2], set, op, result[0],, g0, p0,);
+  ALU1Bit alu1(a[0], b[0], cin, set, op, result[0],, g0, p0,);
   ALU1Bit alu2(a[1], b[1], C1, low, op, result[1],, g1, p1,);
   ALU1Bit alu3(a[2], b[2], C2, low, op, result[2],, g2, p2,);
   ALU1Bit alu4(a[3], b[3], C3, low, op, result[3],, g3, p3, set);
